79eaf6a42dc1a7c5532d219877e3167415765cd6
sky <cl98914@alibaba-inc.com>
2018-11-30 17:44:17 +0800

    feat: particle demo

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 ## egret-learninng
 
+
+- 引入第三方库
+
+拷贝 libx 目录的第三方库，然后刚到与项目名称同级目录
+
+- 先编译
+
+```bash
+egret build
+```
+
+- 运行
+
+```bash
+egret run -a
+```
--- src/Main.ts
+++ src/Main.ts
@@ -1,32 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (c) 2014-present, Egret Technology.
-//  All rights reserved.
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions are met:
-//
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the Egret nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
-//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
-//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//////////////////////////////////////////////////////////////////////////////////////
-
 class Main extends eui.UILayer {
 
     private touchStatus: boolean = false;
@@ -38,6 +9,7 @@ class Main extends eui.UILayer {
     private pointer = new egret.Shape();
     private rainParticle: particle.GravityParticleSystem;
     private ballParticle: particle.GravityParticleSystem;
+    private redParticle: particle.GravityParticleSystem;
     private container: egret.DisplayObjectContainer;
     private rectShape: egret.Shape;
 
@@ -118,6 +90,64 @@ class Main extends eui.UILayer {
         this.createBall();
         // 容器
         this.createContainer();
+        // 对话框
+        this.createPrompt();
+        // 红包雨
+        this.createRedRain();
+    }
+
+    private createRedRain() {
+        const button = new eui.Button();
+        button.label = "红包雨";
+        button.width = 80;
+        button.height = 40;
+        button.x = 10;
+        button.y = 200;
+        this.addChild(button);
+        button.addEventListener(egret.TouchEvent.TOUCH_TAP, () => {
+            const redBg = new egret.Bitmap();
+            redBg.texture = RES.getRes('red_bg_jpg');
+            this.addChildAt(redBg, 0);
+             if (this.redParticle == null) {
+                const texture = RES.getRes("red_icon_png");
+                const config = RES.getRes("lucky_json");
+
+                this.redParticle = new particle.GravityParticleSystem(texture, config);
+                this.redParticle.touchEnabled = true;
+                this.addChildAt(this.redParticle, 1);
+            }
+            this.redParticle.start();
+            setTimeout(() => {
+                this.redParticle.stop(false);
+            }, 5000);
+        }, this);
+    }
+
+    private createPrompt() {
+        const button = new eui.Button();
+        button.label = "对话框";
+        button.width = 80;
+        button.height = 40;
+        button.x = 520;
+        button.y = 150;
+        this.addChild(button);
+        button.addEventListener(egret.TouchEvent.TOUCH_TAP, () => { 
+            const panel = new eui.Panel();
+            panel.x = (this.stage.stageWidth - panel.width) / 2;
+            panel.y = (this.stage.stageHeight - panel.height) / 2;
+            panel.horizontalCenter = 0;
+            panel.verticalCenter = 0;
+            panel.title = "提示";
+
+            const label = new egret.TextField;
+            label.text = 'Welcome Egret World!';
+            label.textAlign = 'center';
+            label.width = 200;
+            label.x = panel.x + 150;
+            label.y = panel.y + 150;
+            panel.addChild(label);
+            this.addChild(panel);
+        }, this);
     }
 
     private createContainer() {
@@ -131,7 +161,7 @@ class Main extends eui.UILayer {
         button.addEventListener(egret.TouchEvent.TOUCH_TAP, () => { 
             if (this.container == null ) {
                 this.container = new egret.DisplayObjectContainer();
-                this.container.x = 400;
+                this.container.x = 300;
                 this.container.y = 10;
                 this.addChild(this.container);
                 
@@ -237,7 +267,7 @@ class Main extends eui.UILayer {
         circle.graphics.lineStyle(5, 0x000000, 1, true)
         circle.graphics.drawCircle(0,0,60);
         circle.graphics.endFill();
-        circle.x = this.stage.stageWidth / 2 ;
+        circle.x = 200 ;
         circle.y = 70;
         this.addChild(circle);
 
@@ -247,8 +277,8 @@ class Main extends eui.UILayer {
         this.pointer.graphics.drawRect(0, 0, 55, 5);
         this.pointer.graphics.endFill();
         this.pointer.anchorOffsetY = this.pointer.height / 2;
-        this.pointer.x = this.stage.stageWidth / 2 ;
-        this.pointer.y = 60;
+        this.pointer.x = 200 ;
+        this.pointer.y = 70;
         this.addChild(this.pointer);
 
         this.timer = new egret.Timer(1000, 0);
@@ -288,8 +318,10 @@ class Main extends eui.UILayer {
         this.logo = new egret.Bitmap();
         this.logo.texture = RES.getRes(name);
         this.logo.touchEnabled = true;
+        this.logo.width = 120;
+        this.logo.height = 120;
         this.logo.x = 10;
-        this.logo.y = 200;
+        this.logo.y = 10;
         this.logo.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this.mouseDown, this);
         this.logo.addEventListener(egret.TouchEvent.TOUCH_END, this.mouseUp, this);
         this.addChild(this.logo);
